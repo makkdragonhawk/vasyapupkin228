a08e506b90a539619219aa2b7688cf889ab0150e
makkdragonhawk <makkdragonhawk@mail.ru>
2016-10-27 13:24:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -35,10 +35,11 @@ name="submit" value="Отправить" />
 </form>
 
 <?php
-Server: sqldatabaseserver4321.database.windows.net,1433 \r\nSQL Database: sqldatabase\r\nUser Name: admin4321\r\n\r\nPHP Data Objects(PDO) Sample Code:\r\n\r\ntry {\r\n   $conn = new PDO ( \"sqlsrv:server = tcp:sqldatabaseserver4321.database.windows.net,1433;
-Database = sqldatabase\", \"admin4321\", \"{your_password_here}\");\r\n   
-$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );\r\n}\r\ncatch ( PDOException $e ) {\r\n   print( \"Error connecting to SQL Server.\" );\r\n   die(print_r($e));\r\n}\r\n\rSQL Server Extension Sample Code:\r\n\r\n$connectionInfo = array(\"UID\" => \"admin4321@sqldatabaseserver4321\", \"pwd\" => \"{your_password_here}\", \"Database\" => \"sqldatabase\", \"LoginTimeout\" => 30, \"Encrypt\" => 1, \"TrustServerCertificate\" => 0);\r\n$serverName = \"tcp:sqldatabaseserver4321.database.windows.net,1433\";
-\r\n$conn = sqlsrv_connect($serverName, $connectionInfo);
+$servername = "sqldatabaseserver4321.database.windows.net";
+$username = "username";
+$password = "password";
+$dbname = "sqldatabase";
+
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
