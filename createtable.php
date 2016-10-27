ae768837b1e42bc6ef7193d2024b86f25f31b023
makkdragonhawk <makkdragonhawk@mail.ru>
2016-10-27 09:59:27 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,7 +1,7 @@
 $host = "sqldatabaseserver4321.database.windows.net,1433";
 $user = "admin4321";
 $pwd = "admin4321";
-$db = "efremovAu91BFbnU";
+$db = "sqldatabaseserver4321";
 $sql = "CREATE TABLE registration_tb(
     id INT NOT NULL IDENTITY(1,1) 
     PRIMARY KEY(id),
