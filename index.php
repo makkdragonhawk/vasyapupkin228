61917347623b3ae81613936eebf9694fe8441142
makkdragonhawk <makkdragonhawk@mail.ru>
2016-10-27 13:08:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -33,6 +33,15 @@ name="email" id="email"/></br>
       <input type="submit" 
 name="submit" value="Отправить" />
 </form>
+try {
+    $conn = new PDO
+( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+    $conn->setAttribute
+( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch(Exception $e){
+    die(var_dump($e));
+}
 
 </body>
 </html>
