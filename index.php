89b1910e91b3a28e667c1bff5e523ee5d4ec84b6
makkdragonhawk <makkdragonhawk@mail.ru>
2016-10-27 13:18:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -33,8 +33,34 @@ name="email" id="email"/></br>
       <input type="submit" 
 name="submit" value="Отправить" />
 </form>
-<?
-include='createtable.php';
+
+<?php
+$servername = "sqldatabaseserver4321.database.windows.net";
+$username = "admin4321";
+$password = "admin4321";
+$dbname = "sqldatabase";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com');";
+$sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Mary', 'Moe', 'mary@example.com');";
+$sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Julie', 'Dooley', 'julie@example.com')";
+
+if (mysqli_multi_query($conn, $sql)) {
+    echo "New records created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 ?>
 </body>
 </html>
