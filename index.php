231a51b926e9c1fd4687b5c3c80ed715c1f9748a
makkdragonhawk <makkdragonhawk@mail.ru>
2016-11-19 09:34:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -26,15 +26,7 @@ border: 0 none; }
 
 </head>
   <body>
-      <?php
-      if(!empty($_POST)) {
-      ?>
-      <pre>
-        <?php print_r($_POST);?>
-      </pre>
-      <?php
-      }
-      ?>
+      
 <h1>Регистрация</h1>
 <p>Заполните ваше имя и адрес электронной почты, затем нажмите кнопку Отправить, чтобы зарегистрироваться.</p>
 <form method="post" action="index.php" 
@@ -46,6 +38,55 @@ name="email" id="email"/></br>
       <input type="submit" 
 name="submit" value="Отправить" />
 </form>
+<?php
+   try {
+    $conn = new PDO
+( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+    $conn->setAttribute
+( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch(Exception $e){
+    die(var_dump($e));
+}
+if(!empty($_POST)) {
+try {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $date = date("Y-m-d");
+    // Insert data
+    $sql_insert = 
+"INSERT INTO registration_tbl (name, email, date) 
+                   VALUES (?,?,?)";
+    $stmt = $conn->prepare($sql_insert);
+    $stmt->bindValue(1, $name);
+    $stmt->bindValue(2, $email);
+    $stmt->bindValue(3, $date);
+    $stmt->execute();
+}
+catch(Exception $e) {
+    die(var_dump($e));
+}
+echo "<h3> Вы зарегистрированы! </h3>";
+}
+$sql_select = "SELECT * FROM registration_tbl";
+$stmt = $conn->query($sql_select);
+$registrants = $stmt->fetchAll(); 
+if(count($registrants) > 0) {
+    echo "<h2>People who are registered:</h2>";
+    echo "<table>";
+    echo "<tr><th>Name</th>";
+    echo "<th>Email</th>";
+    echo "<th>Date</th></tr>";
+    foreach($registrants as $registrant) {
+        echo "<tr><td>".$registrant['name']."</td>";
+        echo "<td>".$registrant['email']."</td>";
+        echo "<td>".$registrant['date']."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "<h3>No one is currently registered.</h3>";
+}
 
+      ?>
 </body>
 </html>
