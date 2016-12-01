b1295e6ebbd079c0379957fe5dae4c770989294b
makkdragonhawk <makkdragonhawk@mail.ru>
2016-12-01 16:07:15 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,4 @@
 <?
 
-$host = "localhost\sqlexpress";
-$user = "makkdragonhawk";
-$pwd = "makkDR3748";
-$db = "sqldatabase2";
-try
-{
-    {  $conn = new PDO("sqlsrv:server = tcp:sqldatabase2.database.windows.net,1433; Database = sqldatabase2", "makkdragonhawk", "makkDR3748");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    {$sql_select = "SELECT * FROM reg_table";
-$stmt = $conn->query($sql_select);
-$registrants = $stmt->fetchAll(); 
-if(count($registrants) > 0) {
-    echo "<h2>People who are registered:</h2>";
-    echo "<table>";
-    echo "<tr><th>Name</th>";
-    echo "<th>Email</th>";
-    echo "<th>Date</th></tr>";
-    foreach($registrants as $registrant) {
-        echo "<tr><td>".$registrant['name']."</td>";
-        echo "<td>".$registrant['email']."</td>";
-        echo "<td>".$registrant['date']."</td></tr>";
-    }
-    echo "</table>";
-} else {
     echo "<h3>No one is currently registered.</h3>";
-}    ?>
+ ?>
