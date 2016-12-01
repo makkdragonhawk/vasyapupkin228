6cb5ce1fd6f0fd90aa55df6beb287eaadfe4c71f
makkdragonhawk <makkdragonhawk@mail.ru>
2016-12-01 15:38:39 +0400

    Update index.php

--- index.php
+++ index.php
@@ -13,14 +13,25 @@ function generateCode($length=6) {
 }
 
 # Соединямся с БД
+
+// Страница авторизации
+
+# Функция для генерации случайной строки
+function generateCode($length=6) {
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHI JKLMNOPRQSTUVWXYZ0123456789";
+    $code = "";
+    $clen = strlen($chars) - 1;
+    while (strlen($code) < $length) {
+            $code .= $chars[mt_rand(0,$clen)];
+    }
+    return $code;
+}
+
 try {
     $conn = new PDO("sqlsrv:server = tcp:sqldatabase2.database.windows.net,1433; Database = sqldatabase2", "makkdragonhawk", "makkDR3748");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+<?
 
 if(isset($_POST['submit']))
 {
@@ -29,9 +40,28 @@ if(isset($_POST['submit']))
     $data = mysqli_fetch_assoc($query);
 
     # Сравниваем пароли
-    if($data['user_password'] === ($_POST['password']))
+    if($data['user_password'] === md5(md5($_POST['password'])))
     {
+        # Генерируем случайное число и шифруем его
+        $hash = md5(generateCode(10));
 
+        if(!@$_POST['not_attach_ip'])
+        {
+            # Если пользователя выбрал привязку к IP
+            # Переводим IP в строку
+            $insip = ", user_ip=INET_ATON('".$_SERVER['REMOTE_ADDR']."')";
+        }
+
+        # Записываем в БД новый хеш авторизации и IP
+        mysqli_query($link, "UPDATE users SET user_hash='".$hash."' ".$insip." WHERE user_id='".$data['user_id']."'");
+
+        # Ставим куки
+        setcookie("id", $data['user_id'], time()+60*60*24*30);
+        setcookie("hash", $hash, time()+60*60*24*30);
+
+        # Переадресовываем браузер на страницу проверки нашего скрипта
+        header("Location: check.php"); exit();
+    }
     else
     {
         print "Вы ввели неправильный логин/пароль";
@@ -40,34 +70,7 @@ if(isset($_POST['submit']))
 ?>
 <form method="POST">
 Логин <input name="login" type="text"><br>
-Пароль <input name="password" type="passw"><br>
+Пароль <input name="password" type="password"><br>
+Не прикреплять к IP(не безопасно) <input type="checkbox" name="not_attach_ip"><br>
 <input name="submit" type="submit" value="Войти">
 </form>
-<?
-// Скрипт проверки
-
-# Соединямся с БД
-$link=mysqli_connect("localhost", "mysql_user", "mysql_password", "testtable");
-
-if (isset($_COOKIE['id']) and isset($_COOKIE['hash']))
-{
-    $query = mysqli_query($link, "SELECT *,INET_NTOA(user_ip) AS user_ip FROM users WHERE user_id = '".intval($_COOKIE['id'])."' LIMIT 1");
-    $userdata = mysqli_fetch_assoc($query);
-
-    if(($userdata['user_hash'] !== $_COOKIE['hash']) or ($userdata['user_id'] !== $_COOKIE['id'])
- or (($userdata['user_ip'] !== $_SERVER['REMOTE_ADDR'])  and ($userdata['user_ip'] !== "0")))
-    {
-        setcookie("id", "", time() - 3600*24*30*12, "/");
-        setcookie("hash", "", time() - 3600*24*30*12, "/");
-        print "Хм, что-то не получилось";
-    }
-    else
-    {
-        print "Привет, ".$userdata['user_login'].". Всё работает!";
-    }
-}
-else
-{
-    print "Включите куки";
-}
-?>
