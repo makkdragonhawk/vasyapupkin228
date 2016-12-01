8fe3f49c111e0132e36c39546b10ae0960d8b6db
makkdragonhawk <makkdragonhawk@mail.ru>
2016-12-01 15:59:00 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,59 +1,26 @@
 <?
-// Страница авторизации
 
-# Функция для генерации случайной строки
-function generateCode($length=6) {
-    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHI JKLMNOPRQSTUVWXYZ0123456789";
-    $code = "";
-    $clen = strlen($chars) - 1;
-    while (strlen($code) < $length) {
-            $code .= $chars[mt_rand(0,$clen)];
-    }
-    return $code;
-}
-
-try {
-    $conn = new PDO("sqlsrv:server = tcp:sqldatabase2.database.windows.net,1433; Database = sqldatabase2", "makkdragonhawk", "makkDR3748");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-if(isset($_POST['submit']))
+$host = "localhost\sqlexpress";
+$user = "makkdragonhawk";
+$pwd = "makkDR3748";
+$db = "sqldatabase2";
+try
 {
-    # Вытаскиваем из БД запись, у которой логин равняеться введенному
-    $query = mysqli_query($conn,"SELECT user_id, user_password FROM users WHERE user_login='".mysqli_real_escape_string($link,$_POST['login'])."' LIMIT 1");
-    $data = mysqli_fetch_assoc($query);
-
-    # Сравниваем пароли
-    if($data['user_password'] === md5(md5($_POST['password'])))
-    {
-        # Генерируем случайное число и шифруем его
-        $hash = md5(generateCode(10));
-
-        if(!@$_POST['not_attach_ip'])
-        {
-            # Если пользователя выбрал привязку к IP
-            # Переводим IP в строку
-            $insip = ", user_ip=INET_ATON('".$_SERVER['REMOTE_ADDR']."')";
-        }
-
-        # Записываем в БД новый хеш авторизации и IP
-        mysqli_query($link, "UPDATE users SET user_hash='".$hash."' ".$insip." WHERE user_id='".$data['user_id']."'");
-
-        # Ставим куки
-        setcookie("id", $data['user_id'], time()+60*60*24*30);
-        setcookie("hash", $hash, time()+60*60*24*30);
-
-        # Переадресовываем браузер на страницу проверки нашего скрипта
-        header("Location: check.php"); exit();
+    {$sql_select = "SELECT * FROM reg_table";
+$stmt = $conn->query($sql_select);
+$registrants = $stmt->fetchAll(); 
+if(count($registrants) > 0) {
+    echo "<h2>People who are registered:</h2>";
+    echo "<table>";
+    echo "<tr><th>Name</th>";
+    echo "<th>Email</th>";
+    echo "<th>Date</th></tr>";
+    foreach($registrants as $registrant) {
+        echo "<tr><td>".$registrant['name']."</td>";
+        echo "<td>".$registrant['email']."</td>";
+        echo "<td>".$registrant['date']."</td></tr>";
     }
-    else
-    {
-        print "Вы ввели неправильный логин/пароль";
-    }
-}
-?>
-<form method="POST">
-Логин <input name="login" type="text"><br>
-Пароль <input name="password" type="password"><br>
-Не прикреплять к IP(не безопасно) <input type="checkbox" name="not_attach_ip"><br>
-<input name="submit" type="submit" value="Войти">
-</form>
+    echo "</table>";
+} else {
+    echo "<h3>No one is currently registered.</h3>";
+}    ?>
