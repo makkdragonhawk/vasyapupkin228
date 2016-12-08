9e40405ea4bb643aa85922f1a586cb1a0b1fa993
makkdragonhawk <makkdragonhawk@mail.ru>
2016-12-08 12:56:54 +0400

    Update index.php

--- index.php
+++ index.php
@@ -35,6 +35,6 @@ if(isset($_POST['submit']))
 ?>
 <form method="POST">
 Логин <input name="login" type="text"><br>
-Пароль <input name="password" type="passw"><br>
+Пароль <input name="password" type="password"><br>
 <input name="submit" type="submit" value="Войти">
 </form>
