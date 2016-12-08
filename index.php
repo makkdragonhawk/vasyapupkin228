3c4be6ccc60cd2f027140e0ff053228c618e01b0
makkdragonhawk <makkdragonhawk@mail.ru>
2016-12-08 13:31:53 +0400

    Update index.php

--- index.php
+++ index.php
@@ -8,19 +8,19 @@ $conn = new PDO("sqlsrv:server = tcp:sqldatabase2.database.windows.net,1433; Dat
 if(isset($_POST['submit']))
 {
     # Вытаскиваем из БД запись, у которой логин равняеться введенному
-  $query = mysqli_query($conn,"SELECT name, passw FROM reg_table WHERE name='".mysqli_real_escape_string($link,$_POST['login'])."' LIMIT 1");
+  $query = mysqli_query($conn,"SELECT name, passw FROM reg_table WHERE name='".mysqli_real_escape_string($conn,$_POST['login'])."' LIMIT 1");
     $data = mysqli_fetch_assoc($query);
 
 
     # Сравниваем пароли
     if($data['passw'] === $_POST['password'])
     {
-        echo $query;
+        echo .$query.;
      print "Вы ввели правильный логин/пароль";
     }
     else
     {
-        echo $query;
+        echo .$query.;
      print "Вы ввели неправильный логин/пароль";
     }
 }
