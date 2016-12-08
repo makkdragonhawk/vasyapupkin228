37b59b2a4d41090b6ffae4f604932fe91ebfe2f4
makkdragonhawk <makkdragonhawk@mail.ru>
2016-12-08 12:45:42 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,41 @@
+<?
+// Страница авторизации
 
-<?php  
-    $conn = new PDO("sqlsrv:server = tcp:sqldatabase2.database.windows.net,1433; Database = sqldatabase2", "makkdragonhawk", "makkDR3748");
+# Функция для генерации случайной строки
+function generateCode($length=6) {
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHI JKLMNOPRQSTUVWXYZ0123456789";
+    $code = "";
+    $clen = strlen($chars) - 1;
+    while (strlen($code) < $length) {
+            $code .= $chars[mt_rand(0,$clen)];
+    }
+    return $code;
+}
+
+# Соединямся с БД
+$conn = new PDO("sqlsrv:server = tcp:sqldatabase2.database.windows.net,1433; Database = sqldatabase2", "makkdragonhawk", "makkDR3748");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-if(isset($_POST['submit'])){ //выполняем нижеследующий код, только если нажата кнопка 
-if(empty($_POST['name'])){ //если переменная логина пуста или не существует  
-echo"Вы не ввели логин"; // выводим сообщение об ошибке  
-  }elseif(!preg_match("/[-a-zA-Z0-9]{3,15}/", $_POST['name'])){ //если переменная не соответствует шаблону -a-zA-Z0-9  
-echo"Вы неправильно ввели логин"; // выводим сообщение об ошибке   
-  }elseif(empty($_POST['passw'])){ //если переменная логина пуста или не существует  
-echo"Вы не ввели пароль"; // выводим сообщение об ошибке  
-  }elseif(!preg_match("/[-a-zA-Z0-9]{3,30}/", $_POST['passw'])){ //если переменная не соответствует шаблону -a-zA-Z0-9  
-echo"Вы неправильно ввели пароль"; // выводим сообщение об ошибке   
-  }else{  
-  $name = $_POST['name']; //присваеваем переменную  
-  $passw = md5($_POST['passw']);//присваеваем переменную и кодируем её в md5 для безопасности  
-  $query = mysql_query("SELECT * FROM `name`  WHERE `name`='$name' AND `passw`='$passw'"); //отправляем запрос на выборку всего содержимого , где поле логин равно переменной $login, а поле password равно переменной $password  
-  $row = mysql_num_rows($query); // считаем количество рядов результата запроса  
-  if($row > 0){ //если их больше 0  
-  echo "Вы успешно авторизовались!"; // выводим сообщение об удачной авторизации!  
-  }else{  
-  echo "Неправильный логин или пароль!"; // выводим сообщение об ошибке!  
-  }  
-   
-  }  
-}  
+
+if(isset($_POST['submit']))
+{
+    # Вытаскиваем из БД запись, у которой логин равняеться введенному
+    $query = mysqli_query($conn,"SELECT name, passw FROM users WHERE user_login='".mysqli_real_escape_string($conn,$_POST['name'])."' LIMIT 1");
+    $data = mysqli_fetch_assoc($query);
+
+    # Сравниваем пароли
+    if($data['passw'] === ($_POST['passw']))
+    {
+       print "Вы вошли";
+    }
+    else
+    {
+        print "Вы ввели неправильный логин/пароль";
+    }
+}
 ?>
+<form method="POST">
+Логин <input name="login" type="text"><br>
+Пароль <input name="password" type="passw"><br>
+Не прикреплять к IP(не безопасно) <input type="checkbox" name="not_attach_ip"><br>
+<input name="submit" type="submit" value="Войти">
+</form>
