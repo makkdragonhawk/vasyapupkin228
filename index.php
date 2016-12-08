7a109320e6267cc6b68b03a70ef86949fb4d22bf
makkdragonhawk <makkdragonhawk@mail.ru>
2016-12-08 12:41:48 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,59 +1,27 @@
-<?
-// Страница авторизации
 
-# Функция для генерации случайной строки
-function generateCode($length=6) {
-    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHI JKLMNOPRQSTUVWXYZ0123456789";
-    $code = "";
-    $clen = strlen($chars) - 1;
-    while (strlen($code) < $length) {
-            $code .= $chars[mt_rand(0,$clen)];
-    }
-    return $code;
-}
-
-# Соединямся с БД
-$conn = new PDO("sqlsrv:server = tcp:sqldatabase2.database.windows.net,1433; Database = sqldatabase2", "makkdragonhawk", "makkDR3748");
+<?php  
+    $conn = new PDO("sqlsrv:server = tcp:sqldatabase2.database.windows.net,1433; Database = sqldatabase2", "makkdragonhawk", "makkDR3748");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-if(isset($_POST['submit']))
-{
-    # Вытаскиваем из БД запись, у которой логин равняеться введенному
-    $query = mysqli_query($conn,"SELECT name, passw FROM users WHERE user_login='".mysqli_real_escape_string($link,$_POST['login'])."' LIMIT 1");
-    $data = mysqli_fetch_assoc($query);
-
-    # Сравниваем пароли
-    if($data['passw'] === md5(md5($_POST['passw'])))
-    {
-        # Генерируем случайное число и шифруем его
-        $hash = md5(generateCode(10));
-
-        if(!@$_POST['not_attach_ip'])
-        {
-            # Если пользователя выбрал привязку к IP
-            # Переводим IP в строку
-            $insip = ", user_ip=INET_ATON('".$_SERVER['REMOTE_ADDR']."')";
-        }
-
-        # Записываем в БД новый хеш авторизации и IP
-        mysqli_query($link, "UPDATE users SET user_hash='".$hash."' ".$insip." WHERE user_id='".$data['user_id']."'");
-
-        # Ставим куки
-        setcookie("id", $data['user_id'], time()+60*60*24*30);
-        setcookie("hash", $hash, time()+60*60*24*30);
-
-        # Переадресовываем браузер на страницу проверки нашего скрипта
-        header("Location: check.php"); exit();
-    }
-    else
-    {
-        print "Вы ввели неправильный логин/пароль";
-    }
-}
+if(isset($_POST['submit'])){ //выполняем нижеследующий код, только если нажата кнопка 
+if(empty($_POST['name'])){ //если переменная логина пуста или не существует  
+echo"Вы не ввели логин"; // выводим сообщение об ошибке  
+  }elseif(!preg_match("/[-a-zA-Z0-9]{3,15}/", $_POST['name'])){ //если переменная не соответствует шаблону -a-zA-Z0-9  
+echo"Вы неправильно ввели логин"; // выводим сообщение об ошибке   
+  }elseif(empty($_POST['passw'])){ //если переменная логина пуста или не существует  
+echo"Вы не ввели пароль"; // выводим сообщение об ошибке  
+  }elseif(!preg_match("/[-a-zA-Z0-9]{3,30}/", $_POST['passw'])){ //если переменная не соответствует шаблону -a-zA-Z0-9  
+echo"Вы неправильно ввели пароль"; // выводим сообщение об ошибке   
+  }else{  
+  $name = $_POST['name']; //присваеваем переменную  
+  $passw = md5($_POST['passw']);//присваеваем переменную и кодируем её в md5 для безопасности  
+  $query = mysql_query("SELECT * FROM `name`  WHERE `name`='$name' AND `passw`='$passw'"); //отправляем запрос на выборку всего содержимого , где поле логин равно переменной $login, а поле password равно переменной $password  
+  $row = mysql_num_rows($query); // считаем количество рядов результата запроса  
+  if($row > 0){ //если их больше 0  
+  echo "Вы успешно авторизовались!"; // выводим сообщение об удачной авторизации!  
+  }else{  
+  echo "Неправильный логин или пароль!"; // выводим сообщение об ошибке!  
+  }  
+   
+  }  
+}  
 ?>
-<form method="POST">
-Логин <input name="login" type="text"><br>
-Пароль <input name="password" type="passw"><br>
-Не прикреплять к IP(не безопасно) <input type="checkbox" name="not_attach_ip"><br>
-<input name="submit" type="submit" value="Войти">
-</form>
