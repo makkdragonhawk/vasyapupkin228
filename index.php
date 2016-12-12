541be9662ffc895e0ff43ab42fc1c409e9bb51ce
makkdragonhawk <makkdragonhawk@mail.ru>
2016-12-12 10:34:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -27,8 +27,8 @@ if(isset($_POST['submit']))
 # Вытаскиваем из БД запись, у которой логин равняеться введенному
 $query = mysqli_query($conn,"SELECT name, passw FROM reg_table WHERE name='".$_POST['login']."' LIMIT 1");
 $data = mysqli_fetch_array($query);
-  print_r($data);
-/*  $sql = "SELECT name, passw FROM reg_table WHERE name=`".$_POST['login']."` LIMIT 1";
+
+/*$sql = "SELECT name, passw FROM reg_table WHERE name=`".$_POST['login']."` LIMIT 1";
   $stmt = $conn->query($sql) or die("query err");
   $stmt->setFetchMode(PDO::FETCH_ASSOC) or die("res err"); 
 */
@@ -57,6 +57,7 @@ print "Вы ввели неправильный логин/пароль";
   echo "пост ".$_POST['password']."<br>";
   */
 }
+  print_r($data);
 ?>
 <form method="POST">
 Логин <input name="login" type="text"><br>
