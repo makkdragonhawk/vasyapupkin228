5971aed463d3585ea83aa9e97c2a2242447cf12a
makkdragonhawk <makkdragonhawk@mail.ru>
2016-12-12 10:14:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,12 @@ if(isset($_POST['submit']))
   $sql = "SELECT name, passw FROM reg_table WHERE name=`".$_POST['login']."` LIMIT 1";
    $stmt = $conn->query($sql);
     $res = $stmt->fetchAll(); 
-   echo "<br>база ".$res['passw']."<br>";
+    if(count($res) > 0) {
+         echo "<br>Логин ".$res['passw']."<br>";
+    } else {
+        echo "no login";
+    }
+  
 /*
 # Сравниваем пароли
 if($data['passw'] === $_POST['password'])
