d4b70386fa40562f9cdd2eb17f89e4f37bae800a
makkdragonhawk <makkdragonhawk@mail.ru>
2016-12-12 09:39:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $conn = new PDO("sqlsrv:server = tcp:sqldatabase2.database.windows.net,1433; Dat
 if(isset($_POST['submit']))
 {
     # Вытаскиваем из БД запись, у которой логин равняеться введенному
-  $query = mysql_query($conn,"SELECT name, passw FROM reg_table WHERE name='".real_escape_string($conn,$_POST['login'])."' LIMIT 1");
+  $query = mysql_query($conn,"SELECT name, passw FROM reg_table WHERE name='"mysql.real_escape_string($conn,$_POST['login'])."' LIMIT 1");
     $data = mysql_fetch_assoc($query);
 
 
